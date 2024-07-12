78de876ca9696ee8513ebd5172331090f6f0831c
Geoffrey Rozinak <mymacstudio@Geoffreys-Mac-Studio.local>
2024-07-12 12:55:26 -0400

    updated resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Rozinak_Resume_July_24.pdf";
+import pdf from "../../Assets/../Assets/Rozinak_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
